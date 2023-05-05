f8f356d93d6936f7ec2b01d6e65f522d2727307b
yuli cuadrado <andrelazaro1967@gmail.com>
2023-05-04 19:05:12 -0500

    manage.py actualizado

--- manage.py
+++ manage.py
@@ -2,9 +2,8 @@
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogpersonal.settings')
     try:
         from django.core.management import execute_from_command_line
